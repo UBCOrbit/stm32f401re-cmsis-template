9b74bf2ce57a6a12857598db8e84f05845079756
Sam Schweigel <s.schweigel@gmail.com>
2019-09-14 16:01:51 -0700

    Blinky

--- src/startup/main.cpp
+++ src/startup/main.cpp
@@ -1,3 +1,17 @@
-int main() {
+#include <stm32f401xe.h>
 
+void sleep() {
+    for (int i = 0; i < 1000000; i++) __asm__("nop");
+}
+
+int main() {
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+    GPIOA->MODER = (GPIOA->MODER & ~GPIO_MODER_MODE5) | GPIO_MODER_MODE5_0;
+
+    while (1) {
+        GPIOA->BSRR = GPIO_BSRR_BS5;
+        sleep();
+        GPIOA->BSRR = GPIO_BSRR_BR5;
+        sleep();
+    }
 }
